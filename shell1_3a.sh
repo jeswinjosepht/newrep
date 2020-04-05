00029003b953ccefd79c9962a59ec66dccf691b2
“Jeswin <jeswinjosepht2016@gmail.com>
2020-04-05 17:37:31 +0530

    Second commit

---
+++ shell1_3a.sh
@@ -0,0 +1,11 @@
+echo "Enter the file name: "
+read file
+if [ -f $file ]
+then
+echo $file "---> It is a ORDINARY FILE."
+elif [ -d $file ]
+then
+echo $file "---> It is a DIRECTORY."
+else
+echo $file "---> It is something else."
+fi
